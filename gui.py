eaae1bb234dd8b14c973fbe6ab5bc8d0a784ad98
Jero11twMac <jero11tw@gmail.com>
2024-03-28 12:08:36 -0500

    Changes in GUI(made grid for check_boxes)

--- gui.py
+++ gui.py
@@ -7,9 +7,13 @@ customtkinter.set_default_color_theme("dark-blue")
 # tkinter window
 # app = tkinter.Tk()
 
-# window sets
+# Creating window
 app = customtkinter.CTk()
-app.geometry("720x480")
+
+# window settings
+height= app.winfo_screenheight()
+width= app.winfo_screenwidth() // 2
+app.geometry("%dx%d" % (width,height))
 app.title("Hawk's meals")
 app.configure(fg_color="yellow")
 
@@ -27,15 +31,28 @@ day.pack(pady=10)
 # black frame
 blck_frame = customtkinter.CTkFrame(app, width=600, height=400)
 blck_frame.pack()
-blck_frame.pack_propagate(False)
+# blck_frame.pack_propagate(False)
+blck_frame.grid_propagate(False)
+
+# grid for blck_frame
+blck_frame.grid_columnconfigure((0, 1, 2), weight=1)
+blck_frame.grid_rowconfigure((0, 1, 2), weight=1)
 
 # Entry Calories (Example)
 entry_calories = customtkinter.CTkEntry(blck_frame, corner_radius=10, placeholder_text="Enter meal", placeholder_text_color='white')
-entry_calories.pack(pady=10)
+# entry_calories.pack(pady=10)
+entry_calories.grid(row=0, column=1)
+
+# checkbox list frame
+boxes_frame = customtkinter.CTkFrame(blck_frame, width=400, height=200, bg_color='yellow')
+# boxes_frame.pack()
+# boxes_frame.pack_propagate(False)
+boxes_frame.grid(row=1, column=1)
 
 # # Button results
 but = customtkinter.CTkButton(blck_frame, text="Results")
-but.pack(padx=10, pady=10, side='bottom')
+# but.pack(padx=10, pady=10, side='bottom')
+but.grid(row=2, column=1)
 
 # def checkbox_event():
 #     print("checkbox toggled, current value:", check_var.get())
